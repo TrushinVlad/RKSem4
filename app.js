cdf26461a70ec9388f4d091ae43ac9154d8a3b9d
Jason Scavenger <39012389+TrushinVlad@users.noreply.github.com>
2022-05-24 13:15:37 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,43 +1,39 @@
 const express = require('express');
+const req = require('express/lib/request');
 const app = express();
-const jsonParser = express.json();
+//const jsonParser = express.json();
 
 app.use(express.static(__dirname + '/styles'));
-
+const urlencodedParser = express.urlencoded({extended: false});
 
 app.listen(5500, () => {
     console.log("Сервер запущен по адресу http://localhost:5500");
 });
 
-app.get('/', (req, res) => {
-    console.log(new Date() + ": обработался запрос, GET '/'.");
-    res.sendFile(__dirname + "/index.html");
-  });
+//app.get('/', (req, res) => {
+//    console.log(new Date() + ": обработался запрос, GET '/'.");
+//    res.sendFile(__dirname + "/index.html");
+//  });
 
-app.post('/', (req, res) => {
-    console.log(new Date() + ": POST-запрос, объект:" + req);
+app.post('/reqres', urlencodedParser, (request, res) => {
+    console.log(new Date() + ": POST-запрос, объект:" + request);
+
+    if(!request.body) 
+    {
+        let js = {
+            status: "Error 400",
+            message: "Неверный тип данных"
+        };
+        res.send(js);
+        return;
+    }
+   
+    console.log(request.body);
     
-    console.log(req.params);
-    //console.log(req.json);
-    if (!req.header) {
-        console.log("Ошибка 400. Нет заголовка.");
-        //let respjson = JSON.parse('{"status": "error", "message": "Неверный тип данных"}');
-        //res.end(respjson);
-        return res.sendStatus(400);
-    }
-    if (!req.is('application/json')) {
-        console.log("Ошибка 400. Неверный тип данных.");
-        //let respjson = JSON.parse('{"status": "error", "message": "Неверный тип данных"}');
-        //res.end(respjson);
-        return res.sendStatus(400);
-    }
-    //if (!req)
-    //console.log(req.body);
-    return;
-    if (!req.body) {
-        console.log("Ошибка 400. Нет тела.");
-        let respjson = JSON.parse('{"status": "error", "message": "Неверный тип данных"}');
-        res.end(respjson);
-        return res.sendStatus(400);
-    }
+    let JSON = {
+        status: "ok",
+        data: request.body
+    };
+    res.send(JSON);
+
 });
